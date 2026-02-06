41262319bfb60eea627dcca9fc5b7da99ba2b6a6
Rodricxzel <braianascanio70@gmail.com>
2026-02-05 23:13:24 -0400

    HU74

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.db.base import Base
+from app.db.session import engine
+from app.api.endpoints import login, users
+
+# Create tables
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+app.include_router(login.router, tags=["login"])
+app.include_router(users.router, prefix="/users", tags=["users"])
 
 @app.get("/")
 def read_root():
